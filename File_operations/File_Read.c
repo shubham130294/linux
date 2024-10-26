c615e9bab21348a8985958ef846c9ebe30a4a0b7
SHUBHAM SHARMA <shubham_sharma_94@icloud.com>
2024-10-26 19:41:44 +0530

    File Systems

--- File_operations/File_Read.c
+++ File_operations/File_Read.c
@@ -1,3 +1,12 @@
+/*
+ssize_t read(int fd, void buf[.count], size_t count);
+Return Type : Actual no. of bytes read from the File
+Argument1 :Opened FD 
+Argument2 : Buffer address where the data has to be stored
+Argument3 : No. of bytes to be fetched 
+*/
+
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<fcntl.h>
@@ -7,9 +16,12 @@
 int main()
 {
 	int FD,sz;
-	char Buffer[10];
+	char Buffer[20];
 
 	FD =open("Image.txt",O_RDWR|O_CREAT,S_IRWXU|S_IRWXG|S_IROTH);
+        #ifdef P1
+        /*P1 macro is used to demonstrate a program where all successive reads are of 5 bytes and the source file has more than 15 bytes*/
+        {
 	sz = read(FD,Buffer,5);
 	printf("CALL 1 : No . of bytes read = %d \n",sz);
 		for(int i = 0 ; i < sz ; i++)
@@ -39,6 +51,38 @@ int main()
                         printf("---%c---",Buffer[i]);
                 }
         printf("\n");
+        }
+        #endif
+	sz = read(FD,Buffer,7);
+	printf("CALL 1 : No . of bytes read = %d \n",sz);
+		for(int i = 0 ; i < sz ; i++)
 
+		{
+
+			printf("%c",Buffer[i]);
+		}
+	printf("\n");
+
+        sz = read(FD,Buffer,20);
+        printf("CALL 2 : No . of bytes read = %d \n",sz);
+  		for(int i = 0 ; i < sz ; i++)
+                
+                {
+
+                        printf("%c",Buffer[i]);
+                }
+        printf("\n");
+
+        sz = read(FD,Buffer,12);
+        printf("CALL 3 : No . of bytes read = %d \n",sz);
+                for(int i = 0 ; i < sz ; i++)
+
+                {
+
+                        printf("%c",Buffer[i]);
+                }
+        printf("\n");
+        
+        
 return 0;
 }
