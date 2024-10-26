9c75af182b2f8bd200daa88e1562cadd5be535cc
SHUBHAM SHARMA <58881296+shubham130294@users.noreply.github.com>
2024-10-26 13:08:20 +0530

    Add files via upload

---
+++ File_Open.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main()
+{
+/* FD :File Descriptor  */
+int FD;
+
+FD = open("IMAGE.jpg",O_RDONLY);
+
+if(FD == -1)
+{
+	printf("Error No :%d\n",errno);
+	perror("ERROR : " );
+}
+else 
+{
+        printf("File Present");
+
+}
+
+return 0; 
+
+}
+
